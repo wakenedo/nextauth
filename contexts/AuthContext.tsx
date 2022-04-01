cca8dbd12ca0f931c9427471070ac8b41ffccd34
wakenedo <a_nuner@outlook.com>
2022-04-01 16:46:44 -0300

    Installed Axios
    Created a api.ts passing the baseURL to http//localhost:3333 where the mock server will be running, the mock server is also in my github so if you wanna test this project just clone it from there.

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
     email: string
@@ -16,11 +17,21 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({ children } : AuthProviderProps) {
+export function AuthProvider({ children }: AuthProviderProps) {
     const isAuthenticated = false
 
-    async function signIn({email, password} : SignInCredentials) {
-        console.log({email, password});
+    async function signIn({ email, password }: SignInCredentials) {
+        try {
+            const response = await api.post('sessions', {
+                email : email,
+                password : password,
+            })
+
+            console.log(response.data)
+
+        } catch (error) {
+            console.log(error)
+        }
     }
     return (
         <AuthContext.Provider value={{ signIn, isAuthenticated }}>
